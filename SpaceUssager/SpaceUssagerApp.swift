a1f54ed0a3795449a0911d67ba033bc53bc70810
Danny Vaca <danny270793@icloud.com>
2025-11-06 15:31:06 -0500

    Initial Commit

---
+++ SpaceUssager/SpaceUssagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceUssagerApp.swift
+//  SpaceUssager
+//
+//  Created by dvaca on 6/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SpaceUssagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
